891f0ce7bf88a5acac47c2104cbd92bc306fa360
Chen Peiyi <cpyyy_0614@163.com>
2024-05-20 16:59:43 +0800

    feat(TimeUtils): add time filter to get timestamp of TODAY, LAST_WEEK, LAST_MONTH

---
+++ src/main/java/org/fdu/awt/minifdustudy/utils/TimeFilter.java
@@ -0,0 +1,11 @@
+package org.fdu.awt.minifdustudy.utils;
+
+/**
+ * @author Violette
+ * @date 2024/5/20 16:30
+ */
+public enum TimeFilter {
+    TODAY,
+    LAST_WEEK,
+    LAST_MONTH
+}
--- src/main/java/org/fdu/awt/minifdustudy/utils/TimeUtils.java
+++ src/main/java/org/fdu/awt/minifdustudy/utils/TimeUtils.java
@@ -1,13 +1,47 @@
 package org.fdu.awt.minifdustudy.utils;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 /**
  * @author Violette
  * @date 2024/5/13 23:43
  */
 public class TimeUtils {
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+
     public static Timestamp now() {
         return new Timestamp(System.currentTimeMillis());
+    }
+
+    public static Timestamp getFromDateBasedOnFilter(TimeFilter filter) {
+        Calendar cal = Calendar.getInstance();
+        resetTime(cal);
+
+        switch (filter) {
+            case TODAY -> {
+                // 返回今天00:00:00的时间戳
+                return new Timestamp(cal.getTimeInMillis());
+            }
+            case LAST_WEEK -> {
+                // 返回上周同一天00:00:00的时间戳
+                cal.add(Calendar.WEEK_OF_MONTH, -1);
+                return new Timestamp(cal.getTimeInMillis());
+            }
+            case LAST_MONTH -> {
+                // 返回上个月同一天00:00:00的时间戳
+                cal.add(Calendar.MONTH, -1);
+                return new Timestamp(cal.getTimeInMillis());
+            }
+            default -> throw new IllegalArgumentException("Invalid time filter");
+        }
+    }
+
+    private static void resetTime(Calendar cal) {
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
     }
 }
