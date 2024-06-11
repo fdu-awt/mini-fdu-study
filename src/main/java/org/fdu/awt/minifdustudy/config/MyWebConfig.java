7e7fe4dd17204766a3a80bdfab0a0e9034dd4e66
zmark <351479188@qq.com>
2024-06-11 15:31:26 +0800

    chore: 由网关统一处理跨域问题

--- src/main/java/org/fdu/awt/minifdustudy/config/MyWebConfig.java
+++ src/main/java/org/fdu/awt/minifdustudy/config/MyWebConfig.java
@@ -1,7 +1,6 @@
 package org.fdu.awt.minifdustudy.config;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,17 +9,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @SpringBootConfiguration
 public class MyWebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOriginPatterns("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
-
-    /**
-     * TODO: Gateway - token验证
-     */
 }
